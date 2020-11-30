37fcfce93bafd8f900d07b8b1e43fe5527ef3e3d
Francesco Minei <francesco.minei.studenti@isii.it>
2020-11-30 10:15:51 +0100

    Bug fix

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     ?>
     <div class="Wrapper">
         <div class="SideBar">
-        <a href="index.php?ID=1"><img src="IMG/Faq_Logo.png" alt="Logo.png"></a>
+        <a href="Home"><img src="IMG/Faq_Logo.png" alt="Logo.png"></a>
         <h1 id="Title">Categorie</h1>
             <input name="search" id="Search" type="text" placeholder="Titolo..." >   
             <button name="ok" type="submit" onclick=EditUrlTitle()><i class="fas fa-search"></i></button>
@@ -32,15 +32,15 @@
                     if(!isset($_SESSION['Username'])){
                 ?>
                 <div class="logout">
-                    <a href="login.html"><i class="fas fa-sign-in-alt"></i></a>
+                    <a href="Login"><i class="fas fa-sign-in-alt"></i></a>
                 </div>
                 <?php } else { ?>
                     <div class="AdminControl">
                     <h1 id="AdminControl">Admin</h1>
-                    <li id="AdminStuff"><a href="admin/AggiungiAdmin.php">Aggiungi admin</a></li>
+                    <li id="AdminStuff"><a href="Admin/AggiungiAdmin">Aggiungi admin</a></li>
                     <li id="AdminStuff"><a href="admin/AggiungiCategoria.php">Aggiungi categoria</a></li>
                     <li id="AdminStuff"><a href="admin/AggiungiDomanda.php">Aggiungi domanda e risposta</a></li>
-                    <?php if(!(isset($_GET['Tag'])||isset($_GET['Title']))){?>
+                    <?php if(!(isset($_GET['Tag'])||!isset($_GET['Title'])||!isset($_GET['ID']))){?>
                     <form action="admin/EditQuestion.php" method="post">
                     <input type="submit" value="Modifica"></br>
                     <select name="Make" id="AdminStuff" name="Category" id="cmbMake" size="5">
@@ -49,7 +49,7 @@
                     if($_GET['ID']!=null)
                         echo LoadTitleWithId((int)$_GET['ID']);
                     else
-                        echo LoadTitleWithId(1);
+                        echo '<option>Impossibile modificare</option>'
                     ?>
                     </select></li>
                     <?php }?>
@@ -74,7 +74,11 @@
                     echo LoadQuestions((int)$_GET['ID']);
             }
             else
-                echo LoadQuestions(1);
+                echo /*LoadQuestions(1);*/ '<h1>Home page</h1><p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus laoreet non curabitur gravida. 
+                Quisque non tellus orci ac. Mattis aliquam faucibus purus in massa tempor nec feugiat nisl. Amet commodo nulla facilisi nullam vehicula ipsum. Cursus mattis molestie a iaculis at erat pellentesque adipiscing. 
+                Fusce ut placerat orci nulla pellentesque dignissim enim sit. Libero id faucibus nisl tincidunt eget. Blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque. Neque gravida in fermentum et sollicitudin ac.
+                 Nec feugiat in fermentum posuere urna nec tincidunt praesent. Faucibus pulvinar elementum integer enim neque volutpat ac. Massa sapien faucibus et molestie ac feugiat sed. 
+                Et malesuada fames ac turpis egestas sed.</p>';
             }
         }
 
--- php/login.php
+++ php/login.php
@@ -12,7 +12,7 @@
         session_start();
         $_SESSION['Username']=$user;
         $_SESSION['Password']=$psw;
-        header('Location: ../index.php');
+        header('Location: ../Home');
     }
     else{
         echo "Wrong username or password";
